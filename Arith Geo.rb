46c975f7144112175268d2d4ad7b7982c28e9558
JEON JEUNGHWAN <wmdghks@gmail.com>
2015-07-31 01:20:15 +0900

    Create Arith Geo.rb

---
+++ Arith Geo.rb
@@ -0,0 +1,14 @@
+def ArithGeo(arr)
+
+    if((arr[1]-arr[0]) == (arr[-1]-arr[-2]))
+      return "Arithmetic"
+    
+    elsif (arr[1]/arr[0]) == (arr[-1]/arr[-2])
+      return -1
+    end
+  
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+ArithGeo(STDIN.gets)           
