0c8ddaf9bda0243fff23ced904ef619384efd272
JEON JEUNGHWAN <wmdghks@gmail.com>
2015-07-31 01:34:32 +0900

    Create  Array Addition I.rb

---
+++ Array Addition I.rb
@@ -0,0 +1,9 @@
+def ArrayAdditionI(arr)
+
+  return arr[0] == arr[1]+arr[2]+arr[-1]+arr[-2] 
+    
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+ArrayAdditionI(STDIN.gets)           
